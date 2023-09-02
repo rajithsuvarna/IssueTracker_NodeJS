81ff64d865ef2f2d67c1cf44b46f76ac5e847312
rajithsuvarna <rajithsuvarna49@gmail.com>
2023-09-02 20:38:02 +0530

    basic structure of the project without filter options

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,13 @@
-module.exports.home = function (req, res) {
-  return res.render("home", { title: "Issue Tracker | Home" });
+const Project = require("../models/project");
+module.exports.home = async function (req, res) {
+  try {
+    let projects = await Project.find({});
+    return res.render("home", {
+      title: "Issue Tracker | Home",
+      all_projects: projects,
+    });
+  } catch (err) {
+    console.log("Error:", err);
+    return;
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const homeController = require("../controllers/home_controller");
 
 router.get("/", homeController.home);
+router.use("/project", require("./project"));
 
 module.exports = router;
