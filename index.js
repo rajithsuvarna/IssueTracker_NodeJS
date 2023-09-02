1330b2e7f113a57195b546668e7249485731777f
rajithsuvarna <rajithsuvarna49@gmail.com>
2023-09-02 13:01:39 +0530

    setup partials and Layouts

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const port = 8000;
 const db = require("./config/mongoose");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
+
+app.use(expressLayouts);
+
+app.use(express.urlencoded());
+app.use(express.static("./assets"));
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
   if (err) {
